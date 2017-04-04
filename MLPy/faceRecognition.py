b1fcc63c1b214c40f6aadf1d2543e5249baf5dd9
Mr.Finger <zhentoufei@163.com>
2017-04-04 20:08:02 +0800

    3

--- MLPy/faceRecognition.py
+++ MLPy/faceRecognition.py
@@ -69,4 +69,20 @@ def plot_gallery(images, titles, h,w,n_row=3,n_col=4):
     plt.figure(figsize=(1.8*n_col,2.4*n_row))
     plt.subplot_adjust(builtin_function_or_method=0, left=.01,right=.99,hspace=.35)
     for i in range(n_row*n_col):
+        plt.subplot(n_row, n_col, i+1)
+        plt.imshow(images[i].reshape((h,w)),cmap=plt.cm.gcay)
+        plt.title(titles[i], size[12])
+        plt.xticks(())
+        plt.yticks(())
 
+def title(y_pred, y_test, target_names, i):
+    pred_name=target_names[y_pred[i].rsplit(' ',1)[-1]]
+    true_name=target_names[y_test[i].rsplit(' ',1)[-1]]
+    return 'predicted:%s\ntrue:     %s' %(pred_name, true_name)
+
+prediction_titles=[title(y_pred,y_test, target_names,i) for i in range(y_pred.shape[0])]
+plot_gallery(X_test, prediction_titles,h,w)
+
+eigenface_titles=["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+plot_gallery(eigenfaces, eigenface_titles,h,w)
+plt.show()
