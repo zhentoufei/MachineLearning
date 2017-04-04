72503b93c4f1d0140906beb8ec53b2a21095816d
Mr.Finger <zhentoufei@163.com>
2017-04-04 17:56:06 +0800

    2

--- MLPy/faceRecognition.py
+++ MLPy/faceRecognition.py
@@ -19,3 +19,54 @@ lfw_people=fetch_lfw_people(min_faces_per_person=70, resize=0.4)
 n_samples, h, w=lfw_people.images.shape
 
 X=lfw_people.data
+n_features=X.shape[1]
+
+y=lfw_people.target
+target_names=lfw_people.target_names
+n_classes=target_names.shope[0]
+
+print("total dataset size:")
+print("n_samples:%d"%n_samples)
+print("n_features:%d"%n_features)
+print("n_classes:%d"%n_classes)
+
+X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.25)
+
+n_componets=150
+
+print("Extracting the top %d eigenfaces from %d faces" % (n_componets, X_train.shape[0]))
+t0=time()
+pca=RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+print("done in %0.3fs"%(time()-t0))
+
+eigenfaces=pca.components_.reshape((n_componets,h,w))
+
+print("Projecting the input data on the eigenfaces orthonormal basis")
+t0=time()
+X_train_pca=pca.transform(X_train)
+X_test_pca=pca.transform(X_test)
+print("done in %0.3fs"%(time()-t0))
+
+print("Fitting")
+t0=time()
+param_grid={'C':[1e3,5e3,1e4,5e4,1e5],
+            'gamma':[0.0001,0.0005,0.001,0.005,0.01,0.1],}
+clf=GridSearchCV(SVC(kernel='rbf',class_weight='auto'), param_grid)
+clf=clf.fit(X_train_pca, y_train)
+print("done in %0.3fs"%(time()-t0))
+print('Best estimator found by grid search!')
+print(clf.best_estimator_)
+
+print("Predict people's name on the test set")
+t0=time()
+y_pred=clf.predict(X_test_pca)
+print('done in %0.3fs'%(time()-t0))
+
+print(classification_report(y_test, y_pred, target_names=target_names))
+print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
+
+def plot_gallery(images, titles, h,w,n_row=3,n_col=4):
+    plt.figure(figsize=(1.8*n_col,2.4*n_row))
+    plt.subplot_adjust(builtin_function_or_method=0, left=.01,right=.99,hspace=.35)
+    for i in range(n_row*n_col):
+
